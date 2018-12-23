f6ecb2da62ceeebbd98b2a909325bb32bc8f0119
Kevin Gregory <medkg15@gmail.com>
2018-12-23 18:38:40 -0500

    Initial program, error handling, token definition.

--- SharpLox/Program.cs
+++ SharpLox/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SharpLox
 {
-    class Program
+    public class Program
     {
+        private static bool hadError;
+
         static void Main(string[] args)
         {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: sharplox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string filename)
+        {
+            var script = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, filename));
+            Run(script);
+            if (hadError)
+            {
+                Environment.Exit(65);
+            }
+        }
+
+        private static void RunPrompt()
+        {
+            while(true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    break;
+                }
+                Run(input);
+                hadError = false;
+            }
+        }
+
+        private static void Run(string script)
+        {
+            var scanner = new Scanner();
+            var tokens = scanner.ScanTokens(script);
+
+            foreach(var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        private static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.WriteLine($"[line {line}] Error {where}: {message}");
+            hadError = true;
         }
     }
 }
---
+++ SharpLox/TokenType.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLox
+{
+    public enum TokenType
+    {
+        // single-character tokens
+        LeftParen,
+        RightParen,
+        LeftBrace,
+        RightBrace,
+        Comma,
+        Dot,
+        Minus,
+        Plus,
+        Semicolon,
+        Slash,
+        Star,
+
+        // one or two character tokens
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
+
+        // literals
+        Identifier,
+        String,
+        Number,
+
+        // keywords
+        And,
+        Class,
+        Else,
+        False,
+        Fun,
+        For,
+        If,
+        Nil,
+        Or,
+        Print,
+        Return,
+        Super,
+        This,
+        True,
+        Var,
+        While,
+
+        Eof
+    }
+}
