04446f5df2122d4d8bdebec75edbd92eca6fc625
Kevin Gregory <medkg15@gmail.com>
2018-12-23 22:29:54 -0500

    T4 template for generating AST classes.

---
+++ SharpLox/AbstractSyntaxTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SharpLox
+{
+	public abstract class Expression
+	{
+		public class Binary : Expression
+		{
+			public Binary(
+			Expression left,
+			Token @operator,
+			Expression right
+			)
+			{
+				this.Left = left;
+				this.Operator = @operator;
+				this.Right = right;
+			}
+
+			public Expression Left { get; set; }
+			public Token Operator { get; set; }
+			public Expression Right { get; set; }
+		} 
+		public class Grouping : Expression
+		{
+			public Grouping(
+			Expression expression
+			)
+			{
+				this.Expression = expression;
+			}
+
+			public Expression Expression { get; set; }
+		} 
+		public class Literal : Expression
+		{
+			public Literal(
+			Object value
+			)
+			{
+				this.Value = value;
+			}
+
+			public Object Value { get; set; }
+		} 
+		public class Unary : Expression
+		{
+			public Unary(
+			Token @operator,
+			Expression right
+			)
+			{
+				this.Operator = @operator;
+				this.Right = right;
+			}
+
+			public Token Operator { get; set; }
+			public Expression Right { get; set; }
+		} 
+	} 
+} 
